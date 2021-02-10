bfd4a7e16585716542ba82cad9a8eef0e5e34554
Juan Arino Sales <punkyfer666@gmail.com>
2021-02-10 19:09:25 +0100

    Update settings.py

--- docker_build/website/tradingplatform/tradingplatform/settings.py
+++ docker_build/website/tradingplatform/tradingplatform/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mjy@^s$wd$cd16jz%_&$ao4xibhuv9=^5=9g-hq6*1y4s@l)ep'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
